3384021e8f2e97d46fa2d10222246f9f05fe516f
Juliane Maran <julianemaran@gmail.com>
2022-10-22 11:22:04 -0300

    java web v1

---
+++ java-web/src/main/java/com/jvm/web/javaweb/JavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.jvm.web.javaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApplication.class, args);
+	}
+
+}
